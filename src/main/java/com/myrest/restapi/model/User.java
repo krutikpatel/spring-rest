931826bc0c708042b33bb13e02f3c08832ee70df
Krutik Patel <meetkrutik@gmail.com>
2020-05-01 22:55:43 -0700

    adding email validations

--- src/main/java/com/myrest/restapi/model/User.java
+++ src/main/java/com/myrest/restapi/model/User.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -20,6 +22,8 @@ public class User {
 	 * knote: Javax validation
 	 */
 	@NotNull
+	@Email
+	@NotEmpty
 	String email;
 	
 	/*
